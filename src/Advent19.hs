3b72935c5ebea51d5e7b51f02da2a592ec7e4e4d
Lyndon Maydwell <lyndon@sordina.net>
2021-01-03 11:00:06 +1100

    19 solution

--- src/Advent19.hs
+++ src/Advent19.hs
@@ -1,62 +1,65 @@
 module Advent19 where
 
-day19 = undefined
-day19b = undefined
+import Data.Either (rights)
+import Text.Parsec (try, choice, string, eof, parse, Parsec)
+import Control.Arrow (second, Arrow((&&&)))
+import Data.List.Split (splitOn)
+import Data.Char (isDigit)
+import Data.Maybe (fromJust)
 
--- hoe2 -m 'Text.Parsec Debug.Trace' '
--- let
--- foo :: [(String,[[String]])] -> Parsec String () ()
--- foo r =
--- 	let
--- 	a = M.fromList (map g r)
--- 	f n@(x:xs)
--- 		| isDigit x = a ! n
--- 		| otherwise = () <$ string (init xs)
--- 	g = second (choice . map (try . mapM_ f))
--- 	m ! k = fromJust $ M.lookup k m
--- 	in a ! "0" *> eof
--- in
--- length .  rights
--- . (\(s,p) -> map (parse p "rules") s)
--- . (last &&& foo . map ((init . head &&& splitOn ["|"] . tail) . words) .  head)
--- . splitOn [""]
--- '
+import Control.Monad.Trans.Free ( FreeT(FreeT), iterTM, FreeF(Pure, Free) )
+import Data.Tree ( Tree(Node), drawTree )
+import Control.Monad.State ( mfilter, StateT(StateT, runStateT) )
+import Control.Lens ( (&), (.~), Field2(_2) )
+import Control.Applicative ( Alternative(empty, (<|>)) )
 
--- #!/bin/sh
+import qualified Data.List as L
+import qualified Data.Map  as M
 
+day19
+    = show . length .  rights
+    . (\(s,p) -> map (parse p "rules") s)
+    . (last &&& foo . map ((init . head &&& splitOn ["|"] . tail) . words) .  head)
+    . splitOn [""] . lines
+    where
+    foo :: [(String,[[String]])] -> Parsec String () ()
+    foo r = a ! "0" *> eof
+      where
+      a = M.fromList (map g r)
+      f n@(x:xs)
+        | isDigit x = a ! n
+        | otherwise = () <$ string (init xs)
+      g = second (choice . map (try . mapM_ f))
+      m ! k = fromJust $ M.lookup k m
 
--- hoe2 -m 'Control.Monad.Trans.Free Control.Monad.State Data.Tree' '
--- let
+day19b
+    = show . length -- (\x -> map drawTree x ++ [show $ length x])
+    . map (fst . head)
+    . filter (not . null)
+    . (\(s,p) -> map (parseString p) s)
+    . (map (++"EOF") . last &&& build . map (sub . (init . head &&& splitOn ["|"] . tail) . words) .  head)
+    . splitOn [""] . lines
 
--- token            = FreeT . pure . Free $ FreeT . pure . Pure
--- parseStream next = runStateT . iterTM (StateT next >>=)
--- parseString      = parseStream (maybe empty pure . Data.List.uncons)
--- char c           = mfilter (==c) token
--- string           = mapM char
--- choice           = foldl (<|>) empty
+    where
 
--- sub t@("8", _) = t & _2 .~ [["42"],["42","8"]]
--- sub t@("11",_) = t & _2 .~ [["42","31"],["42","11","31"]]
--- sub t          = t
+    token            = FreeT . pure . Free $ FreeT . pure . Pure
+    parseStream next = runStateT . iterTM (StateT next >>=)
+    parseString      = parseStream (maybe empty pure . L.uncons)
+    char c           = mfilter (==c) token
+    string           = mapM char
+    choice           = foldl (<|>) empty
 
--- m!k = fromJust $ M.lookup k m
+    sub t@("8", _) = t & _2 .~ [["42"],["42","8"]]
+    sub t@("11",_) = t & _2 .~ [["42","31"],["42","11","31"]]
+    sub t          = t
 
--- n :: [a] -> Bool
--- n = null
+    m ! k = fromJust $ M.lookup k m
 
--- build :: [(String,[[String]])] -> FreeT ((->) Char) [] (Tree String)
--- build r = fmap (Node "0") $ a ! "0" <* string "EOF"
--- 	where
--- 	a = M.fromList (map (second g) r)
--- 	g = choice . map (traverse f)
--- 	f n@(x:xs)
--- 		| isDigit x  = Node n <$> a ! n
--- 		| otherwise  = Node n [] <$ string (init xs)
--- in
--- (\x -> map drawTree x ++ [show $ length x])
--- . map (fst . head)
--- . filter (not . null)
--- . (\(s,p) -> map (parseString p) s)
--- . (map (++"EOF") . last &&& build . map (sub . (init . head &&& splitOn ["|"] . tail) . words) .  head)
--- . splitOn [""]
--- '
+    build :: [(String,[[String]])] -> FreeT ((->) Char) [] (Tree String)
+    build r = fmap (Node "0") $ a ! "0" <* string "EOF"
+        where
+        a = M.fromList (map (second g) r)
+        g = choice . map (traverse f)
+        f n@(x:xs)
+            | isDigit x  = Node n <$> a ! n
+            | otherwise  = Node n [] <$ string (init xs)
