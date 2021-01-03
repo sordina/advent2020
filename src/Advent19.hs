1aad70df6c3721951667a221818e1baec0293cfa
Lyndon Maydwell <lyndon@sordina.net>
2021-01-03 16:22:31 +1100

    notes

--- src/Advent19.hs
+++ src/Advent19.hs
@@ -1,5 +1,7 @@
 module Advent19 where
 
+-- See http://sordina.github.io/blog/2021/01/03/1609638326-advent19b.html for writeup.
+
 import Data.Either (rights)
 import Text.Parsec (try, choice, string, eof, parse, Parsec)
 import Control.Arrow (second, Arrow((&&&)))
