e81de08c125947a292724c06f428610f69680f9f
Lyndon Maydwell <lyndon@sordina.net>
2021-01-06 23:50:33 +1100

    3 docs

--- src/Advent02.hs
+++ src/Advent02.hs
@@ -6,13 +6,28 @@ import Control.Lens.Regex.Text ( groups, regex )
 import qualified Data.Text as T
 import Control.Lens ( (^..) )
 
-day2  = f f2
+-- "Each line gives the password policy and then the password. The password
+--  policy indicates the lowest and highest number of times a given letter must
+--  appear for the password to be valid. For example, 1-3 a means that the
+--  password must contain a at least 1 time and at most 3 times."
+
+day2 = f f2
     where
-    f2 [l,h,c,p] = n >= read (T.unpack l) && n <= read (T.unpack h) where n = T.length (T.filter (== T.index c 0) p)
+    f2 [l,h,c,p] = n c p >= read (T.unpack l) && n c p <= read (T.unpack h)
+    n c p = T.length (T.filter (== T.index c 0) p)
+
+-- "Each policy actually describes two positions in the password, where 1 means
+--  the first character, 2 means the second character, and so on. (Be careful;
+--  Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of
+--  these positions must contain the given letter. Other occurrences of the
+--  letter are irrelevant for the purposes of policy enforcement."
 
 day2b = f f2b
     where
-    f2b [l,h,c,p] = 1 == length (filter (== T.index c 0) [T.index p (pred $ read (T.unpack l)), T.index p (pred $ read (T.unpack h))])
+    f2b [l,h,c,p]
+        = 1 == length (
+            filter (== T.index c 0)
+            [T.index p (pred $ read (T.unpack l)), T.index p (pred $ read (T.unpack h))])
 
 rx = [regex|(\d+)-(\d+) (\S+): (\S+)|]
 
@@ -30,7 +45,6 @@ f x = length . concatMap (filter x . (^.. rx . groups)) . T.lines . T.pack
 -- 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
 -- 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
 -- 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
-
 
 exampleInput =
     "1-3 a: abcde\
--- src/Advent03.hs
+++ src/Advent03.hs
@@ -1,13 +1,49 @@
 
 module Advent03 where
 
+-- "From your starting position at the top-left, check the position that is
+--  right 3 and down 1. Then, check the position that is right 3 and down 1 from
+--  there, and so on until you go past the bottom of the map."
+
 day3 = show . length . filter id . f . map cycle . lines
     where
     f m = zipWith (\x y -> m!!y!!x == '#') [0,3..] [0..length m-1] 
 
+-- What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+-- 
+-- * Right 1, down 1.
+-- * Right 3, down 1. (This is the slope you already checked.)
+-- * Right 5, down 1.
+-- * Right 7, down 1.
+-- * Right 1, down 2.
 
 day3b
     = show . product . f g . map cycle . lines
     where
     f z m = [sum[1|True<-zipWith(\x y->m!!y!!x=='#')i(takeWhile(<length m)j)]|(i,j)<-[z 1,z 3,z 5,z 7,([0..],[0,2..])]]
     g z = ([0,z..],[0..])
+
+-- >>> day3 testInput
+-- "7"
+
+-- "In this example, traversing the map using this slope would cause you to encounter 7 trees."
+
+-- >>> day3b testInput
+-- "336"
+
+-- "In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s)
+--  respectively; multiplied together, these produce the answer 336."
+
+testInput = unlines
+    [ "..##......."
+    , "#...#...#.."
+    , ".#....#..#."
+    , "..#.#...#.#"
+    , ".#...##..#."
+    , "..#.##....."
+    , ".#.#.#....#"
+    , ".#........#"
+    , "#.##...#..."
+    , "#...##....#"
+    , ".#..#...#.#"
+    ]
